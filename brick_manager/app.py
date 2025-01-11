3611b8caf8d29967e39debd761572ff4bd0fc3f0
Holger Kornhaas <Holger@Kornhaas.net>
2025-01-11 17:28:53 +0100

    Fix security finding in flask

--- brick_manager/app.py
+++ brick_manager/app.py
@@ -124,4 +124,4 @@ scheduler.start()
 atexit.register(scheduler.shutdown)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
