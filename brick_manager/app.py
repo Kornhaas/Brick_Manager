641ce58614561406f4c5aadf7410cf5b6596bc61
Holger Kornhaas <Holger@Kornhaas.net>
2025-10-08 10:55:59 +0000

    change port to 5001

--- brick_manager/app.py
+++ brick_manager/app.py
@@ -122,4 +122,4 @@ scheduler.start()
 atexit.register(scheduler.shutdown)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
